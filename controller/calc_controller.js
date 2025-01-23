1c45f66b6a6e04db29499aa2dfdb93b3cf9f7aa9
Juan Coutinho <juan.devx@gmail.com>
2025-01-23 13:05:00 -0300

    (Create) Class and Initialize

--- controller/calc_controller.js
+++ controller/calc_controller.js
@@ -1,3 +1,27 @@
 class CalcController {
-    
+    constructor(){
+       this._displayCalc = "0";
+       this._currentDate;
+       this.initialize;
+    }
+
+    initialize(){
+        
+    }
+
+    get displaycalc(){
+        return this._displayCalc;
+    }
+
+    set displaycalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+       return this._actual_Date;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
 }
