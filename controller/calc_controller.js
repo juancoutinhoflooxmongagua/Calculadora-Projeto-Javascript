2652f1a15135c265cbabc63917513624e52686a3
Juan Coutinho <juan.devx@gmail.com>
2025-01-23 13:22:31 -0300

    (Feat) Add display in calculator

--- controller/calc_controller.js
+++ controller/calc_controller.js
@@ -1,27 +1,33 @@
 class CalcController {
-    constructor(){
-       this._displayCalc = "0";
-       this._currentDate;
-       this.initialize;
+    constructor() {
+        this._displayCalc = "0";
+        this._currentDate = null;
+        this.initialize();
     }
 
-    initialize(){
-        
+    initialize() {
+        let displaycalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displaycalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "12/12/2025"; 
+        timeEl.innerHTML = "15:30";      
     }
 
-    get displaycalc(){
+    get displayCalc() {
         return this._displayCalc;
     }
 
-    set displaycalc(valor){
+    set displayCalc(valor) {
         this._displayCalc = valor;
     }
 
-    get currentDate(){
-       return this._actual_Date;
+    get currentDate() {
+        return this._currentDate;
     }
 
-    set currentDate(valor){
+    set currentDate(valor) {
         this._currentDate = valor;
     }
 }
