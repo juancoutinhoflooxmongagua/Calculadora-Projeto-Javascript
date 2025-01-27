2de2f129e4e9e9e089bd9a33530ddacccf765cdc
Juan Coutinho <juan.coutinho.flooxmongagua@gmail.com>
2025-01-27 13:23:36 -0300

    feat: project finished with try catch

--- controller/calc_controller.js
+++ controller/calc_controller.js
@@ -206,9 +206,19 @@ class CalcController {
 
     getResult(){
 
+        try{
 
+            return eval(this._operation.join(""));
 
-        return eval(this._operation.join(""));
+        }catch(e){
+
+            setTimeout(()=>{
+
+                this.setError();
+
+            }, 1)
+
+        }
 
     }
 
@@ -487,11 +497,14 @@ class CalcController {
 
     set displayCalc(value){
 
-        if (value.toString.length > 10){
+        if (value.toString().length > 10) {
+
             this.setError();
 
-            return false
+            return false;
+
         }
+
         this._displayCalcEl.innerHTML = value;
 
     }
